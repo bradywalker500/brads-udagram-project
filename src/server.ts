f79b8434205785c84849a420e7d54f2b13c29dc3
bradywalker500 <bradywalker500@gmail.com>
2022-07-21 02:00:38 +0100

    eb deploy command tested for second deployment

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("eb deployed - try GET /filteredimage?image_url={{}}")
   } );
   
 
